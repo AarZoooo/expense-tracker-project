79b53652e48d4b05e6c323b97ad23a6a8959e1d8
Aarju Pal <aarju.pal1234@gmail.com>
2024-08-18 19:49:50 +0530

    Edit: Renaming of fields same to firestore

--- app/src/main/java/com/aarzoo/expensetracker/backend/DataModels.kt
+++ app/src/main/java/com/aarzoo/expensetracker/backend/DataModels.kt
@@ -3,20 +3,19 @@ package com.aarzoo.expensetracker.backend
 import com.google.firebase.Timestamp
 
 data class User (
-    val name: String,
-    val email: String,
-    val monthlyLimit: Double
+    val uNAME: String,
+    val uEMAIL: String,
+    val uMONTHLIMIT: Double
 )
 
 data class Category (
-    val name: String,
-    val color: String
+    val cNAME: String,
+    val cCOLOR: String
 )
 
 data class Transaction (
-    val tid: String,
-    val recipient: String,
-    val amount: Double,
-    val dateTime: Timestamp,
+    val tRECIPIENT: String,
+    val tAMOUNT: Double,
+    val tDATETIME: Timestamp,
     val cName: String
 )
